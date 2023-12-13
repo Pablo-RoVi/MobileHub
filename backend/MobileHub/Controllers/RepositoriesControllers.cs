13ad8b5c1065f29afcfd3e6875c08f4579ee9377
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-13 19:44:47 -0300

    feat: Create Repository and Commit endpoint

---
+++ backend/MobileHub/Controllers/RepositoriesControllers.cs
@@ -0,0 +1,44 @@
+using DotNetEnv;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace MobileHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Repositories : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Repository>>> Get()
+        {
+            Env.Load();
+            var client = new GitHubClient(new ProductHeaderValue("MobileHub"));
+            var myToken = Env.GetString("GITHUB_ACCESS_TOKEN");
+            client.Credentials = new Credentials(myToken);
+            
+            var repositories = (await client.Repository.GetAllForUser("Dizkm8")).ToList();
+            return repositories;
+
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Commits : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<GitHubCommit>>> Get()
+        {
+            Env.Load();
+            var client = new GitHubClient(new ProductHeaderValue("MobileHub"));
+            var myToken = Env.GetString("GITHUB_ACCESS_TOKEN");
+            client.Credentials = new Credentials(myToken);
+            
+            var commits = (await client.Repository.Commit.GetAll("Dizkm8", "Hackathon")).ToList();
+            return commits;
+            
+        }
+    }
+}
