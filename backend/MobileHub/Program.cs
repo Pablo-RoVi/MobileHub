9cf90063dc73ac2e4c5a10fbd7b34e241b6abf6a
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-17 00:54:25 -0300

    feat: create database

--- backend/MobileHub/Program.cs
+++ backend/MobileHub/Program.cs
@@ -22,11 +22,11 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite("Data Source=MobileHub.db"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite("Data Source=MobileHub.db"));
 
 var app = builder.Build();
 
